3e186d060ba81de5e667b9b3eb6dabdb45661ed3
kissy <hiroki.kishi@finatext.com>
2023-07-20 18:47:04 +0900

    refactor

---
+++ popup/extract.js
@@ -0,0 +1,42 @@
+(async function extract() {
+  let richText, plainText
+
+  if (/aws.amazon.com$/.test(location.host)) {
+    const title = document.querySelector('title').innerText
+    let roleNameAndAccount = document.querySelector('#nav-usernameMenu').innerText.split(" @ ");
+    if (roleNameAndAccount.length === 1) {
+      roleNameAndAccount = document.querySelector('#nav-usernameMenu').innerText.split("+@+");
+    }
+    const switchRoleURL = `https://signin.aws.amazon.com/switchrole?roleName=${roleNameAndAccount[0]}&account=${roleNameAndAccount[1]}&from=switch-role-ins-chrome-extension`
+
+    richText = [
+      '<ul>',
+      `<li><a href="${location.href}">${title}</a></li>`,
+      `<li><a href="${switchRoleURL}">Switch Role URL</a>(<code>${roleNameAndAccount[0]}@${roleNameAndAccount[1]}</code>)`,
+      '</ul>'
+    ].join("\n")
+
+    plainText = [
+      `- [${title.replace(/ /g, '')}](${location.href})`,
+      "\n",
+      `- [SwitchRoleURL\`${roleNameAndAccount[0]}@${roleNameAndAccount[1]}\`](${switchRoleURL})`
+    ].join("\n")
+
+  } else {
+    const title = document.querySelector('title').innerText
+      .replace(' | finatextgroup Kibela', '') // Kibela
+      .replace(/ · .+/, '') // GitHub
+    const url = location.href
+    const body = `<a href="${url}"/>${title}</a>`
+    richText = plainText = body
+  }
+
+  const item = new ClipboardItem(
+    {
+      'text/html': new Blob([richText], {type: 'text/html'}),
+      'text/plain': new Blob([plainText], {type: 'text/plain'})
+    }
+  )
+  await navigator.clipboard.write([item])
+  return {richText, plainText}
+})()
--- popup/index.js
+++ popup/index.js
@@ -1,17 +1,23 @@
-const message = document.getElementById('message')
-chrome.tabs.query({active: true, currentWindow: true}).then(tabs => {
-  chrome.scripting.executeScript({
-    target: {tabId: tabs[0].id},
-    files: ['popup/copy.js']
-  })
-    .then((results) => {
-      const result = results[0].result
-      const messageText = result.message
-      if (result.success) {
-        message.innerHTML = `<span style="color:green">Copied!<br>${messageText}</span>`
-      } else {
-        message.innerHTML = `<span style="color:red">Faild:<br>${messageText}</span>`
-      }
+(async () => {
+  const message = document.getElementById('message')
+  let results
+  try {
+    const tabs = await chrome.tabs.query({active: true, currentWindow: true})
+    results = await chrome.scripting.executeScript({
+      target: {tabId: tabs[0].id},
+      files: ['popup/extract.js']
     })
-})
+  } catch (e) {
+    message.innerHTML = `<span style="color:red">Faild:<br>${e.message}</span>`
+    return
+  }
+
+  const {richText} = results[0].result
+
+  try {
+    message.innerHTML = `<span style="color:green">Copied!<br>${richText}</span>`
+  } catch (e) {
+    message.innerHTML = `<span style="color:red">Faild:<br>${e.message}</span>`
+  }
+})()
 
